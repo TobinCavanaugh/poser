76c5189f70197c80eba75d9c0ba8017fdcbf42f6
Tobin Cavanaugh <tobincavanaugh@gmail.com>
2024-09-05 11:50:12 -0500

    Fixed str_cmp

--- poser/str/str_cmp.c
+++ poser/str/str_cmp.c
@@ -7,19 +7,8 @@
 #include "str.h"
 
 i32 str_cmp(const char* a, const char* b) {
-    u32 alen = str_len(a);
-    u32 blen = str_len(b);
-
-    u32 min = (alen > blen) ? blen : alen;
-
-    u32 i = 0;
-    for(; i < min; i++)
-    {
-        if(a[i] != b[i])
-        {
-            return a[i] > b[i];
-        }
-    }
-
-    return 0;
+    while (*a == *b++)
+        if (*a++ == '\0')
+            return (0);
+    return (*(u8*)a - *(u8*)(b - 1));
 }
